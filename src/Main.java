989ff0461a2b51facbacd31c10bdfbeb026094ad
pclink74 <pclink74@yandex.ru>
2024-06-10 07:42:47 +0500

    commit_sprint_6_V CheckStyle bugs

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
-import service.Managers;
-import service.TaskManager;
 import model.Epic;
 import model.Status;
 import model.SubTask;
 import model.Task;
+import service.Managers;
+import service.TaskManager;
 
 public class Main {
 
     public static void main(String[] args) {
 
         TaskManager taskManager = Managers.getDefault();
-        Task testTask1=taskManager.create(new Task("Первая задача", Status.NEW, "Это первая задача"));
+        Task testTask1 = taskManager.create(new Task("Первая задача", Status.NEW, "Это первая задача"));
         Task testTask2 = taskManager.create(new Task("Вторая задача", Status.NEW, "Это вторая задача"));
         Epic testEpic1 = taskManager.createEpic((new Epic("первый эпик", "Это первый Эпик")));
-        SubTask testSubTask1=taskManager.createSubTask(new SubTask("Первая подзадача", Status.NEW, "Это первая подзадача", testEpic1));
-        SubTask testSubTask2=taskManager.createSubTask(new SubTask("Вторая подзадача", Status.NEW, "Это вторая подзадача", testEpic1));
+        SubTask testSubTask1 = taskManager.createSubTask(new SubTask("Первая подзадача", Status.NEW, "Это первая подзадача", testEpic1));
+        SubTask testSubTask2 = taskManager.createSubTask(new SubTask("Вторая подзадача", Status.NEW, "Это вторая подзадача", testEpic1));
         Epic testEpic2 = taskManager.createEpic((new Epic("Второй эпик", "Это второй Эпик")));
-        SubTask testSubTask3=taskManager.createSubTask(new SubTask("Третья подзадача", Status.NEW, "Это третья подзадача", testEpic2));
+        SubTask testSubTask3 = taskManager.createSubTask(new SubTask("Третья подзадача", Status.NEW, "Это третья подзадача", testEpic2));
         System.out.println(taskManager.getAllEpics());
         System.out.println(taskManager.getAll());
         System.out.println(taskManager.getAllSubTasks());
