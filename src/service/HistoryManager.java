2f4a8380f31d851c4cd64d41478fc21c504ff4c9
pclink74 <pclink74@yandex.ru>
2024-06-10 07:33:15 +0500

    commit_sprint_6_V CheckStyle bugs

--- src/service/HistoryManager.java
+++ src/service/HistoryManager.java
@@ -5,7 +5,8 @@ import model.Task;
 import java.util.List;
 
 public interface HistoryManager {
-    void add (Task task);
+    void add(Task task);
+
     List<Task> getAllHistory();
 
     void remove(int id);
